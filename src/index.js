9491460ffaf67d6245ab955b55c16964fd04bce3
oussamaAlouat <al317257@uji.es>
2020-08-17 00:37:53 +0200

    added 3 comments like before

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
+  const loop = [];
+  for (let i = 0; i < 3; i++) {
+    loop.push(<CommentDetail/>)
+  }
+
+
   return (
     <div className="ui container comments">
-      <CommentDetail/>
+      {loop}
     </div>
   );
 };
