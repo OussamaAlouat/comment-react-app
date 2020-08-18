58ee240d3563a6f476f6670beb6a2d3607d321cd
Oussama Alouat <oussama.alouat@amaris.com>
2020-08-18 15:49:04 +0200

    Added props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+  const { author, comment, timeAt, image } = props;
    return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()}/>
+        <img alt="avatar" src={image}/>
       </a>
 
       <div className="content">
         <a href="/" className="author">
-          Sam
+          { author }
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00 PM</span>
+   <span className="date">{timeAt}</span>
         </div>
-        <div className="text"> Nice blog post!</div>
+        <div className="text"> { comment }</div>
       </div>
     </div>
    );
--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import CommentDetail from './CommentDetail';
 
 const App = () => {
   const loop = [];
+  const timeAt =() => `Today at ${faker.random.number(12)} :00`;
   for (let i = 0; i < 3; i++) {
-    loop.push(<CommentDetail/>)
+    loop.push(<CommentDetail author={faker.name.firstName()} comment={faker.hacker.phrase()} timeAt={timeAt()} image={faker.image.avatar()}/>)
   }
 
 
